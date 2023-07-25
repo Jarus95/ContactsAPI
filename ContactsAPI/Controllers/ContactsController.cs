917a27e2938819ce6fb25e897b4add0063cda2eb
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-07-25 21:40:59 +0500

    Add ContactController And Add CRUD operations

---
+++ ContactsAPI/Controllers/ContactsController.cs
@@ -0,0 +1,61 @@
+﻿using ContactsAPI.Data;
+using ContactsAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ContactAPIDbContext dbContext;
+        public ContactsController(ContactAPIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetContacts()
+        {
+           return Ok(await dbContext.Contacts.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddContacts(AddContactRequest addContactRequest)
+        {
+            var contact = new Contact()
+            {
+                Id = Guid.NewGuid(),
+                Adress = addContactRequest.Adress,
+                Email = addContactRequest.Email,
+                FullName = addContactRequest.FullName,
+                Phone = addContactRequest.Phone
+            };
+
+            await dbContext.Contacts.AddAsync(contact);
+            await dbContext.SaveChangesAsync();
+
+            return Ok(contact);
+        }
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateContact([FromRoute] Guid id, UpdateContactRequest updateContactRequest)
+        {
+           var contact = await dbContext.Contacts.FindAsync(id);
+
+           if(contact != null)
+           {
+                contact.Adress = updateContactRequest.Adress;
+                contact.Email = updateContactRequest.Email;
+                contact.FullName = updateContactRequest.FullName;
+                contact.Phone = updateContactRequest.Phone;
+                await dbContext.SaveChangesAsync();
+                return Ok(contact);
+           }
+
+           return NotFound();
+        }
+    }
+}
